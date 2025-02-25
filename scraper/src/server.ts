c35283d2fde936c85d0ec64109e8c9e1dd4cc963
Han Nie <nie.han@northeastern.edu>
2025-02-24 18:08:00 -0800

    Add script and logic in endpoint

--- scraper/src/server.ts
+++ scraper/src/server.ts
@@ -1,12 +1,30 @@
 import express from 'express';
 import router from './routes/routes';
 import 'dotenv/config';
+import { chromium } from 'playwright';
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use('/', router);
+
+export let browser;
+
+async function launchBrowser() {
+    browser = await chromium.launch({
+        headless: false,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-background-timer-throttling',
+            '--disable-backgrounding-occluded-windows',
+            '--disable-renderer-backgrounding',
+        ],
+    });
+}
+
+launchBrowser();
 
 app.listen(process.env.PORT || 8081, () => {
     console.log(`Server is running on port ${process.env.PORT || 8081}`);
