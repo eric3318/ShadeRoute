e8737b90d00b881d93a751d972e52210989f0ffe
Han Nie <nie.han@northeastern.edu>
2025-02-25 19:07:23 -0800

    Add redis pub/sub and websocket to notify job progress

--- server/index.ts
+++ server/index.ts
@@ -3,11 +3,26 @@ import express, { Request, Response } from 'express';
 import { Input, Data } from './lib/types';
 import { requestQueue } from './config/mq';
 import { v4 as uuidv4 } from 'uuid';
-
+import redis from './config/db';
+import cors from 'cors';
+import io from './config/socket';
 const app = express();
+
+app.use(cors({
+    origin: '*',
+}));
 
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
+
+redis.subscribe('job:*', (err, count) => {
+    if (err) console.error(err);
+});
+
+redis.on('message', (channel, message) => {
+    const jobId = channel.split('job:')[1];
+    io.to(jobId).emit('job:completed', jobId);
+});
 
 const PARTITION_SIZE = 10;
 
@@ -20,13 +35,14 @@ function splitWorkload(data: Data) {
 }
 
 app.post('/', async(req: Request<{},{}, Input>, res: Response) => {
-    const jobId = uuidv4();
+    const baseJobId = uuidv4();
     const { data, timestamp } = req.body;
     const partitions = splitWorkload(data);
     for (let i = 0; i < partitions.length; i++) {
-        await requestQueue.add('request', { timestamp, data: partitions[i] }, { jobId: `${jobId}-result_${i}` });
+        await requestQueue.add('request', { timestamp, data: partitions[i] }, { jobId: `${baseJobId}-result_${i}` });
     }
-    res.status(200).json({ message: 'Processing started', jobId });
+    await redis.set(`job:${baseJobId}:total`, partitions.length);
+    res.status(200).json({ message: 'Processing started', jobId: baseJobId });
 });
 
 app.listen(process.env.PORT, () => {
