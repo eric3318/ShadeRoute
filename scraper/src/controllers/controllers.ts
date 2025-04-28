f068311ea323d5b1740b255bf75a1467d951cd5e
Han Nie <nie.han@northeastern.edu>
2025-04-27 22:47:59 -0700

    Switch to Puppeteer due to issues enabling hardware acceleration with Playwright on gpu instances in Kubernetes

--- scraper/src/controllers/controllers.ts
+++ scraper/src/controllers/controllers.ts
@@ -1,69 +1,59 @@
 import path from 'path';
 import { Input, Output } from '../lib/types';
-import { browser } from '../server';
+import { browser } from '..';
+import { fileURLToPath } from 'url';
+import type { BrowserContext } from 'puppeteer';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 declare global {
     interface Window {
         inputData: Input;
         outputData: Output;
+        MAPBOX_CRED: string;
+        SHADEMAP_CRED: string;
     }
 }
 
-// export const getData = async (req: Request<{}, {}, Input>, res: Response) => {
-//     const { timestamp, data, jobId, resultId } = req.body;
-
-//     if (!timestamp || !data || !jobId || !resultId) {
-//         res.status(400).send({ message: 'Invalid request' });
-//         return;
-//     }
-
-//     res.status(202).send({ message: 'Processing started' });
-
-//     // add more validation logic here
-
-//     const result = (await processInBackground(req.body)) as Output;
-//     if (!result) {
-//         return;
-//     }
-
-//     const luaScript = `for i = 1, #KEYS - 1 do
-//                             redis.call("hset", KEYS[1], KEYS[i+1], ARGV[i])
-//                         end`;
-
-//     const keys = Object.keys(result);
-//     const values = Object.values(result).map((valueArr) => valueArr.join(','));
-
-//     await redis.eval(luaScript, keys.length + 1, `job:${jobId}:result_${resultId}`, ...keys, ...values);
-// };
-
 export const processInBackground = async (input: Input) => {
-    let context;
+    let context: BrowserContext | null = null;
     try {
-        context = await browser.newContext();
+        context = await browser.createBrowserContext();
         const page = await context.newPage();
 
-        await page.setViewportSize({ width: 1920, height: 1080 });
+        page.setViewport({ width: 1920, height: 1080 });
 
-        await page.addInitScript(`
-        window.MAPBOX_CRED = '${process.env.MAPBOX_ACCESS_TOKEN}';
-        window.SHADEMAP_CRED = '${process.env.SHADEMAP_API_KEY}';
-        window.inputData = ${JSON.stringify(input)};
-    `);
+        page.on('console', (msg) => {
+            console.log(msg.text());
+        });
 
         const htmlPath = path.join(__dirname, '../scripts/shadow.html');
-        await page.goto(`file://${htmlPath}`);
+
+        await page.evaluateOnNewDocument(
+            (input, mapbox, shademap) => {
+                window.MAPBOX_CRED = mapbox;
+                window.SHADEMAP_CRED = shademap;
+                window.inputData = input;
+            },
+            input,
+            process.env.MAPBOX_ACCESS_TOKEN as string,
+            process.env.SHADEMAP_API_KEY as string,
+        );
+
+        await page.goto(`file://${htmlPath}`, {
+            waitUntil: 'networkidle0',
+        });
 
         await page.waitForFunction(() => window.outputData !== undefined, {
-            timeout: 60000, // 1 minute timeout
+            timeout: 30000,
         });
 
         const result = await page.evaluate(() => window.outputData);
 
-        await context.close();
-
         return result;
     } catch (error) {
-        console.error(error);
+        console.error('Error in Puppeteer process:', error);
         return {};
     } finally {
         if (context) {
---
+++ scraper/src/index.ts
@@ -0,0 +1,130 @@
+import express from 'express';
+import 'dotenv/config';
+import puppeteer, { Browser } from 'puppeteer';
+import redis from './config/db';
+import { Worker } from 'bullmq';
+import { processInBackground } from './controllers/controllers';
+import { Input } from './lib/types';
+import { Response, Request } from 'express';
+
+const app = express();
+
+export let browser: Browser;
+let worker: Worker;
+
+async function initDevBrowser() {
+    browser = await puppeteer.launch({
+        headless: false,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-background-timer-throttling',
+            '--disable-backgrounding-occluded-windows',
+            '--disable-renderer-backgrounding',
+        ],
+        defaultViewport: {
+            width: 1920,
+            height: 1080,
+        },
+    });
+}
+
+async function initBrowser() {
+    browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--headless=new',
+            '--use-angle=vulkan',
+            '--enable-features=Vulkan',
+            // '--disable-vulkan-surface',
+            // '--enable-unsafe-webgpu',
+            // '--disable-search-engine-choice-screen',
+            // '--ash-no-nudges',
+            // '--no-first-run',
+            // '--disable-features=Translate',
+            // '--no-default-browser-check',
+            // '--window-size=1920,1080',
+            '--allow-chrome-scheme-url',
+        ],
+        defaultViewport: {
+            width: 1920,
+            height: 1080,
+        },
+    });
+}
+
+const luaScript = `
+    local currentCount = redis.call("incr", KEYS[1])
+    if currentCount == tonumber(KEYS[2]) then
+        redis.call("publish", KEYS[3], "completed")
+    end`;
+
+async function initWorker() {
+    worker = new Worker<Input>(
+        'request-queue',
+        async (job) => {
+            if (!job.id) {
+                return;
+            }
+            console.log('Processing job', job.id);
+            const { timestamp } = job.data;
+            const data = JSON.parse((await redis.get(`job:${job.id}`)) || '[]');
+
+            if (data.length === 0) {
+                console.log('No data found');
+                return;
+            }
+
+            const result = await processInBackground({
+                timestamp,
+                data,
+            });
+            return result;
+        },
+        { connection: redis, concurrency: 5 },
+    );
+
+    worker.on('completed', async (job, result) => {
+        if (job && job.id) {
+            console.log('Job completed', job.id);
+            const baseJobId = job.id.split(':')[0];
+            await redis.hset(`job:${baseJobId}:result`, result);
+            const totalPartitions = job.data.total;
+            await redis.eval(luaScript, 3, `job:${baseJobId}:completed`, Number(totalPartitions), `job:${baseJobId}`);
+        }
+    });
+
+    worker.on('failed', (job) => {
+        if (job) {
+            console.error(`Job ${job.id} failed with error ${job.failedReason}`);
+        }
+    });
+}
+
+async function init() {
+    try {
+        if (process.env.NODE_ENV === 'development') {
+            await initDevBrowser();
+        } else {
+            await initBrowser();
+        }
+        await initWorker();
+    } catch (error) {
+        console.error('Initialization error:', error);
+    }
+}
+
+init();
+
+app.get('/health', async (req: Request, res: Response) => {
+    if (!browser || !worker) {
+        res.status(500).json({ status: 'error', message: 'Not Ok' });
+        return;
+    }
+    res.status(200).json({ status: 'success', message: 'Ok' });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Worker is running on port ${process.env.PORT}`);
+});
