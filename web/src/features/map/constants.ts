4673044047083a9842df6011a5a11fb45e3657c3
Han Nie <nie.han@northeastern.edu>
2025-06-04 22:33:31 -0700

    Change enum to as const object

--- web/src/features/map/constants.ts
+++ web/src/features/map/constants.ts
@@ -1,5 +1,7 @@
-export enum Mode {
-  RUNNING = 'running',
-  WALKING = 'walking',
-  BIKING = 'biking',
-}
+export const Mode = {
+  RUNNING: 'running',
+  WALKING: 'walking',
+  BIKING: 'biking',
+} as const;
+
+export type Mode = (typeof Mode)[keyof typeof Mode];
